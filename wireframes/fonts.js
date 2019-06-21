38a26f9b3cfaf7367f45be2e077fb1d0d1b6f6b4
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2019-06-21 14:07:51 +0100

    home page wireframe small screen

---
+++ wireframes/fonts.js
@@ -0,0 +1,10 @@
+WebFontConfig = {google: {families: ['Maiden Orange','Stylish']}};
+(function(d) {
+  var wf = d.createElement('script'), s = d.scripts[0]
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
+  wf.type = 'text/javascript'
+  wf.async = 'true'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(wf, s)
+  })(document)
